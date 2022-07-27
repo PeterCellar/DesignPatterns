86e0c168f722839f24bde7d7eadae7cec0fb3331
cellar <pcellar@acmark.com>
2022-07-27 09:41:09 +0200

    Simple Singleton design pattern

---
+++ Singleton/LoadBalancer.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Singleton
+{
+    public class LoadBalancer
+    {
+        static LoadBalancer instance;
+        List<string> servers = new List<string>();
+        Random random = new Random();
+
+        // Lock synchronization object
+        private static object locker = new object();
+
+        protected LoadBalancer()
+        {
+            servers.Add("server1");
+            servers.Add("server2");
+            servers.Add("server3");
+        }
+
+        /*
+         Support multithread applications through
+         "double checked locking" pattern which
+         once the instance exists avoid locking
+         each time the method is invoked
+         */
+        public static LoadBalancer GetLoadBalancer()
+        {
+            if(instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                    {
+                        instance = new LoadBalancer();
+                    }
+                }
+            }
+
+            return instance;
+        }
+
+        // Random load balancer
+        public string Server
+        {
+            get 
+            { 
+                int r = random.Next(servers.Count);
+                return servers[r].ToString();
+            }
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿
+/*
+    Singleton design pattern ensures a class has only one instance
+    and provides a global point of access to it.
+
+    Participants
+        Singleton : defines an instance operation that lets clients access
+                    its unique instance. Responsible for creating and maintaining its own unique instance
+ */
+
+namespace Singleton
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+
+            if (b1 == b2) { Console.WriteLine("Same instance"); }
+
+            // Load balance 15 server requests
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 10; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch request to: " + server);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
