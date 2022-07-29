f8cbe0aaba8b0e63184850aa13c33f409019d4c9
cellar <pcellar@acmark.com>
2022-07-29 10:08:10 +0200

    Simple Memento design pattern

--- Memento/Program.cs
+++ Memento/Program.cs
@@ -4,7 +4,12 @@
     an objects internal state so that the object can be restored to this state later.
 
     Participants
-        
+        Memento    : stores internal state of the Originator object. Protects against access
+                     by objects of other than the originator
+        Originator : creates a memento containing a snapshot of its current internal state
+                     uses the memento to restore its internal state
+        Caretaker  : is responsible for the mementos sagekeeping, never operates on or examines
+                     the contents of a memento
  */
 
 namespace Memento
@@ -13,7 +18,19 @@ namespace Memento
     {
         public static void Main(string[] args)
         {
-            
+            SalesProspect s = new SalesProspect();
+            s.Name = "Noel van Halen";
+            s.Phone = "(412) 256-0990";
+            s.Budget = 25000.0;
+
+            ProspectMemory m = new ProspectMemory();
+            m.Memento = s.SaveMemento();
+
+            s.Name = "Leo Welch";
+            s.Phone = "(310) 209-7111";
+            s.Budget = 1000000.0;
+
+            s.RestoreMemento(m.Memento);
 
             Console.ReadKey();
         }
---
+++ Memento/ProspectMemory.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Memento
+{
+    // The Caretaker class
+    public class ProspectMemory
+    {
+        Memento memento;
+
+        public Memento Memento
+        {
+            get { return memento; }
+            set { memento = value; }
+        }
+    }
+}
