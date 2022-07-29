0358e6d1733e84d3cdf7ef53d5216b5bbf0ba804
cellar <pcellar@acmark.com>
2022-07-29 15:56:59 +0200

    Simple Strategy desing pattern

---
+++ Strategy/Program.cs
@@ -0,0 +1,24 @@
+﻿
+/*
+    Strategy desing pattern defines a family of algorithms, encapsulate each one,
+    and make them interchangable.
+
+    Participants
+        Strategy          : declares an interface common to all supported algortihms. Context uses
+                            this interface to call the algorithm defined by a Concrete Strategy
+        Concrete Strategy : implements the algorithm using the Strategy interface
+        Context           : is configured with a Concrete Strategy object, maintains a reference to a Strategy object
+                            may define an interface that lets Strategy access its data
+ */
+
+namespace Strategy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            Console.ReadKey();
+        }
+    }
+}
