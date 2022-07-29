d8c5206fe785c1c1433a5ac93c06d605a6a2717c
cellar <pcellar@acmark.com>
2022-07-29 09:51:32 +0200

    Simple Memento design pattern

---
+++ Memento/Program.cs
@@ -0,0 +1,21 @@
+﻿
+/*
+    Memento design pattern without violating encapsulation, captures and externalizes
+    an objects internal state so that the object can be restored to this state later.
+
+    Participants
+        
+ */
+
+namespace Memento
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            Console.ReadKey();
+        }
+    }
+}
